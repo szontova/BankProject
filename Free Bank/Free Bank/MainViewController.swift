e36a4b179ac3856e6384dbd6ab677c7eb7c30f45
szontova <62010270+szontova@users.noreply.github.com>
2021-02-04 17:25:43 +0300

    Add files

---
+++ Free Bank/Free Bank/MainViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MainViewController.swift
+//  Free Bank
+//
+//  Created by Пользователь on 3.02.21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MainViewController: it works")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func moveToSignInButton(_ sender: UIButton) {
+        performSegue(withIdentifier: "signInSegue", sender: nil)
+    }
+}
---
+++ Free Bank/Free Bank/SignInViewController.swift
@@ -0,0 +1,101 @@
+//
+//  ViewController.swift
+//  Free Bank
+//
+//  Created by Пользователь on 1.02.21.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginErrorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let touchErrorLabel = UITapGestureRecognizer(target: self, action: #selector(hidErrorLabel(_:)))
+        
+        touchErrorLabel.numberOfTapsRequired = 1
+        touchErrorLabel.numberOfTouchesRequired = 1
+        
+        loginErrorLabel.addGestureRecognizer(touchErrorLabel)
+        loginErrorLabel.isUserInteractionEnabled = true
+        loginErrorLabel.isHidden = true
+        // Do any additional setup after loading the view.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    func showAlertError(title: String, message: String){
+        let alertError = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let attributedString = NSAttributedString(string: title, attributes: [
+            NSAttributedString.Key.font : UIFont.systemFont(ofSize: 15),
+            NSAttributedString.Key.foregroundColor : UIColor.red
+        ])
+        alertError.setValue(attributedString, forKey: "attributedTitle")
+        alertError.view.tintColor = UIColor.black
+        let okAction = UIAlertAction(title: "OK", style: .default){_ in}
+        alertError.addAction(okAction)
+        present(alertError, animated: true, completion: nil)
+    }
+    
+    @IBAction func signInButton(_ sender: UIButton) {
+    
+        let login = loginTextField.text ?? ""
+        let password = passwordTextField.text ?? ""
+        
+            if login == "" || password == "" {
+                showAlertError(title: "Ошибка", message: "Введите логин и пароль.")
+                //loginErrorLabel.text = "Данные не введены"
+                //loginErrorLabel.isHidden = false
+            }
+            
+            else{
+                loginErrorLabel.isHidden = true
+                let allowLetters: Range<Character> = "A"..<"z"
+                let allowSymbols: Set<Character> = [".","_"]
+                
+                if login.count < 8 || login.count > 50 {
+                    showAlertError(title: "Ошибка", message: "Неверный формат логина. Проверьте данные.")
+                }
+                else {
+                    for symb in login {
+                        if  (!allowLetters.contains(symb)) && (!allowSymbols.contains(symb)) {
+                            showAlertError(title: "Ошибка", message: "Неверно введен логин. Проверьте данные.")
+                            //loginErrorLabel.text = "Данные введены неверно"
+                            //loginErrorLabel.isHidden = false
+                            break
+                        }
+                    }
+                }
+                
+                //сделать пароль
+//                else if  (!allowLetters.contains(symb)) && (!allowSymbols.contains(symb)) {
+//                    showAlertError(title: "Ошибка", message: "Неверно введен логин. Проверьте данные.")
+//                    //loginErrorLabel.text = "Данные введены неверно"
+//                    //loginErrorLabel.isHidden = false
+//                    break
+//                }
+            }
+    }
+    
+    @IBAction func hidErrorLabel(_ gesture: UITapGestureRecognizer){
+        loginErrorLabel.isHidden.toggle()
+        loginTextField.text = ""
+    }
+    
+}
+
+extension SignInViewController: UITextFieldDelegate{
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+

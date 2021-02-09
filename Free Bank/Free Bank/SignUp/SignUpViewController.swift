f724229effbd0eac3a7a6ef5409ffb0a36676551
Valeriya <valeri1605@mail.ru>
2021-02-09 12:40:40 +0300

    change the appearance of alert when changing the theme

--- Free Bank/Free Bank/SignUp/SignUpViewController.swift
+++ Free Bank/Free Bank/SignUp/SignUpViewController.swift
@@ -54,14 +54,25 @@ class SignUpViewController: UIViewController {
     
     func showAlertError(title: String, message: String){
         let alertError = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let attributedString = NSAttributedString(string: title, attributes: [
+        alertError.view.subviews.first?.subviews.first?.subviews.first?.backgroundColor = UIColor.white
+        
+        let attributedStringForTitle = NSAttributedString(string: title, attributes: [
             NSAttributedString.Key.font : UIFont.systemFont(ofSize: 15),
             NSAttributedString.Key.foregroundColor : UIColor.red
         ])
-        alertError.setValue(attributedString, forKey: "attributedTitle")
-        alertError.view.tintColor = UIColor.black
+        alertError.setValue(attributedStringForTitle, forKey: "attributedTitle")
+        
+        let attributedStringForMessage = NSAttributedString(string: message, attributes: [
+            NSAttributedString.Key.font : UIFont.systemFont(ofSize: 13),
+            NSAttributedString.Key.foregroundColor : UIColor.black
+        ])
+        alertError.setValue(attributedStringForMessage, forKey: "attributedMessage")
+     
+    
         let okAction = UIAlertAction(title: "OK", style: .default){_ in}
+        okAction.setValue(UIColor.black, forKey: "titleTextColor")
         alertError.addAction(okAction)
+        //поговорить по поводу полосочки
         present(alertError, animated: true, completion: nil)
     }
     

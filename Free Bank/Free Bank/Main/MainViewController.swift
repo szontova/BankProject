252728afd15ecc2be999ea64e30ba8abeae839cf
Valeriya <valeri1605@mail.ru>
2021-02-18 16:54:03 +0300

    add collectionView with images

--- Free Bank/Free Bank/Main/MainViewController.swift
+++ Free Bank/Free Bank/Main/MainViewController.swift
@@ -8,10 +8,18 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var viewFofCollectionView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        collectionView.register(CollectionViewCell.nib(), forCellWithReuseIdentifier: CollectionViewCell.identifier)
+        
         //print("MainViewController: it works")
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
     
 
@@ -44,4 +52,41 @@ class MainViewController: UIViewController {
         guard segue.identifier == "unwindToMainVCSegue" else {return}
         guard let _ = segue.destination as? SignInViewController else {return}
     }
+}
+
+extension MainViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        print("you tapped me")
+    }
+}
+
+extension MainViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.identifier, for: indexPath) as! CollectionViewCell
+        cell.configure(with: UIImage(named: "cards")!)
+        return cell
+    }
+    
+}
+
+extension MainViewController: UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: viewFofCollectionView.bounds.width - 10, height: viewFofCollectionView.bounds.width)
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
+    }
+    
 }
---
+++ Free Bank/Free Bank/Main/Static Pages/CollectionView/CollectionViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  CollectionViewCell.swift
+//  Free Bank
+//
+//  Created by Пользователь on 18.02.21.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    static let identifier = "CollectionViewCell"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        self.imageView.image = nil
+    }
+    
+    public func configure(with image: UIImage) {
+        //imageView.layer.cornerRadius = 20.0
+        imageView.image = image
+    }
+    
+    static func nib() -> UINib {
+        return UINib(nibName: identifier, bundle: nil)
+    }
+}

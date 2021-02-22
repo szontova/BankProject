9e968a6c330e21b557968df706e96ee1e79e103b
Valeriya <valeri1605@mail.ru>
2021-02-22 16:58:05 +0300

    new file for collectionView functions

--- Free Bank/Free Bank/Main/MainViewController.swift
+++ Free Bank/Free Bank/Main/MainViewController.swift
@@ -14,9 +14,12 @@ class MainViewController: UIViewController {
     
     private let images = ["cards", "bank", "successfulPeople"]
     
+    func getImages() -> [String] {
+        return images
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         
         collectionView.register(CollectionViewCell.nib(), forCellWithReuseIdentifier: CollectionViewCell.identifier)
                 
@@ -24,16 +27,6 @@ class MainViewController: UIViewController {
         collectionView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     @IBAction func moveToSignInButton(_ sender: UIButton) {
         performSegue(withIdentifier: "toSignInSegue", sender: nil)
@@ -43,10 +36,8 @@ class MainViewController: UIViewController {
         performSegue(withIdentifier: "toExchangeRateSegue", sender: nil)
     }
     
-    
     @IBAction func unwindToMainVCFromExchangeRateTVC(segue: UIStoryboardSegue){
         guard segue.identifier == "unwindToMainFromExRateSegue" else {return}
-        print(segue.destination)
         guard let _ = segue.destination as? ExchangeRateTableViewController else {return}
     }
     
@@ -54,80 +45,4 @@ class MainViewController: UIViewController {
         guard segue.identifier == "unwindToMainVCSegue" else {return}
         guard let _ = segue.destination as? SignInViewController else {return}
     }
-}
-
-extension MainViewController: UICollectionViewDelegate {
-    
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        collectionView.deselectItem(at: indexPath, animated: true)
-        //go to//
-        print("you tapped me \(indexPath.row)")
-    }
-}
-
-extension MainViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return images.count + 1
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.identifier, for: indexPath) as! CollectionViewCell
-        cell.configure(with: UIImage(named: images[indexPath.row % images.count])!)
-        return cell
-    }
-    
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-        let pageFloat = (scrollView.contentOffset.x / scrollView.frame.size.width)
-        let pageInt = Int(round(pageFloat))
-             
-        switch pageInt {
-            case 0:
-                collectionView.scrollToItem(at: [0, images.count], at: .left, animated: false)
-            case images.count :
-                collectionView.scrollToItem(at: [0, 0], at: .left, animated: false)
-            default:
-                break
-        }
-    }
-}
-
-extension MainViewController: UICollectionViewDelegateFlowLayout {
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        let width = viewFofCollectionView.bounds.width
-        let height = viewFofCollectionView.bounds.height
-        
-        if height > width {
-            return CGSize(width: width - 10 , height: width - 10)
-        } else {
-            return CGSize(width: height - 10 , height: height - 10)
-        }
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        
-        let width = viewFofCollectionView.bounds.width
-        let height = viewFofCollectionView.bounds.height
-        
-        if height < width {
-            return (width - height + 10)
-        }
-        return 10
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        var indent: CGFloat = 5.0
-        
-        let width = viewFofCollectionView.bounds.width
-        let height = viewFofCollectionView.bounds.height
-        
-        if height < width {
-            indent = (width - height) / 2 + 5
-        }
-        return UIEdgeInsets(top: 0, left: indent, bottom: 0, right: indent)
-    }
-    
-    
 }

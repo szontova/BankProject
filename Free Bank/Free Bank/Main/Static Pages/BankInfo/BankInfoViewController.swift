49cfa5e23ce72f5e5c980915dab93769ae8d8e30
Valeriya <valeri1605@mail.ru>
2021-02-25 18:27:33 +0300

    console commit bankInfo.storyboard

--- Free Bank/Free Bank/Main/Static Pages/BankInfo/BankInfoViewController.swift
+++ Free Bank/Free Bank/Main/Static Pages/BankInfo/BankInfoViewController.swift
@@ -11,7 +11,7 @@ class BankInfoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     

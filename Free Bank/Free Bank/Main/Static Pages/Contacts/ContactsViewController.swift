32507fb71103776891d99904de642758788e773a
Alexandra <shzontova@gmail.com>
2021-02-26 19:10:46 +0300

    add link social media

--- Free Bank/Free Bank/Main/Static Pages/Contacts/ContactsViewController.swift
+++ Free Bank/Free Bank/Main/Static Pages/Contacts/ContactsViewController.swift
@@ -15,15 +15,68 @@ class ContactsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func openSocialMedia(url: String){
+        if let url = URL(string: url) { UIApplication.shared.open(url, options: [:], completionHandler: nil)}
     }
-    */
-
+    
+    func callNumber(number: String){
+        if let url: NSURL = URL(string:"TEL://"+number) as NSURL?  { UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)}
+    }
+    
+    @IBAction func callLeraButton(_ sender: UIButton) {
+         callNumber(number: "+375291951959")
+    }
+    
+    @IBAction func facebookLeraButton(_ sender: UIButton) {
+        openSocialMedia(url: "https://www.facebook.com/lera1605")
+    }
+    
+    @IBAction func vkLeraButton(_ sender: UIButton) {
+        openSocialMedia(url: "https://vk.com/lerachubakova")
+    }
+    
+    @IBAction func instagramLeraButton(_ sender: UIButton) {
+        openSocialMedia(url: "https://www.instagram.com/lerachubakova/")
+    }
+    
+    @IBAction func linkedLeraButton(_ sender: UIButton) {
+        openSocialMedia(url: "http://linkedin.com/in/lerachubakova")
+    }
+    
+    @IBAction func telegramLeraButton(_ sender: UIButton) {
+        openSocialMedia(url: "https://t.me/lera_chubakova")
+    }
+    
+    @IBAction func skypeLeraButton(_ sender: UIButton) {
+        openSocialMedia(url: "https://join.skype.com/invite/mQabsV93UMho")
+    }
+    
+    @IBAction func callSashaButton(_ sender: UIButton) {
+        callNumber(number: "+375295721707")
+    }
+    
+    @IBAction func facebookSashaButton(_ sender: UIButton) {
+        openSocialMedia(url: "https://www.facebook.com/")
+    }
+    
+    @IBAction func vkSashaButton(_ sender: UIButton) {
+        openSocialMedia(url: "https://vk.com/shzontova")
+    }
+    
+    @IBAction func instagramSashaButton(_ sender: UIButton) {
+        openSocialMedia(url: "https://www.instagram.com/shzontova/")
+    }
+    
+    @IBAction func githubSashaButton(_ sender: UIButton) {
+        openSocialMedia(url: "https://github.com/szontova/BankProject")
+    }
+    
+    @IBAction func telegramSashaButton(_ sender: UIButton) {
+        openSocialMedia(url: "https://t.me/shzontova")
+    }
+    
+    @IBAction func skypeSashaButton(_ sender: UIButton) {
+        openSocialMedia(url: "https://join.skype.com/invite/fZdS99SiOzGz")
+    }
+    
 }

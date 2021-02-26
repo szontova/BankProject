f1daca5e7abd341d35a758e0b0b11c636e3ea2db
Alexandra <shzontova@gmail.com>
2021-02-26 11:56:38 +0300

    add func call and func send mail

--- Free Bank/Free Bank/Main/Static Pages/BankInfo/BankInfoViewController.swift
+++ Free Bank/Free Bank/Main/Static Pages/BankInfo/BankInfoViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MessageUI
 
 class BankInfoViewController: UIViewController {
 
@@ -25,5 +26,36 @@ class BankInfoViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    func callNumber(phone: String){
+        if let url: NSURL = URL(string: "TEL://" + phone) as NSURL?  { UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)}
+    }
+        
+    @IBAction func shortCallButton(_ sender: UIButton) {
+        callNumber(phone: "123")
+    }
+    @IBAction func fullCallButton(_ sender: UIButton) {
+        callNumber(phone: "+375295721707")
+    }
+    
+    @IBAction func sendMailButton(_ sender: UIButton) {
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.setSubject("Поддержка приложения FreeBank")
+            mail.setToRecipients(["freebank.by@gmail.com"])
+            mail.setMessageBody("Здравствуйте! Дайте денег, пожалуйста.", isHTML: false)
+            present(mail, animated: true)
 
+        } else {
+            showAlertError(message: "Ошибка при отправлении письма.")
+        }
+    }
+    
+}
+
+extension BankInfoViewController: MFMailComposeViewControllerDelegate{
+        func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+                dismiss(animated: true)
+            }
 }

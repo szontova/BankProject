9f6df9585607278516324a8ddd976de91d4b741a
Valeriya <valeri1605@mail.ru>
2021-03-01 11:58:51 +0300

    Add new files for home view controllers

---
+++ Free Bank/Free Bank/Home/AccountsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AccountsViewController.swift
+//  Free Bank
+//
+//  Created by Пользователь on 1.03.21.
+//
+
+import UIKit
+
+class AccountsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

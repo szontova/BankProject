6528707388d9453d4390eb56cfa5ca7e3fd59480
Valeriya <valeri1605@mail.ru>
2021-03-01 18:58:35 +0300

    tableView in account page

--- Free Bank/Free Bank/Home/AccountsViewController.swift
+++ Free Bank/Free Bank/Home/AccountsViewController.swift
@@ -10,14 +10,21 @@ import UIKit
 class AccountsViewController: UIViewController {
     
     @IBOutlet weak var navigationBar: UINavigationBar!
+    @IBOutlet weak var accountsTableView: UITableView!
     
     private var individual: Individual?
     private var organization: Organization?
-      
+    
+    private var accounts: [Account] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         transparentNavBar(navigationBar)
+        
+        accountsTableView.delegate = self
+        accountsTableView.dataSource = self
+        
     }
 
     @IBAction func addAccountButton(_ sender: UIButton) {
@@ -34,4 +41,22 @@ extension AccountsViewController: OrgIndivid {
         self.organization = org
     }
     
+}
+
+extension AccountsViewController: UITableViewDelegate {}
+
+extension AccountsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let count = individual?.accounts?.count ?? 0
+        return count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = self.accountsTableView.dequeueReusableCell(withIdentifier: "accountCell")! as UITableViewCell
+       // let acc =  individual?.accounts
+       
+        cell.textLabel?.text = "la"
+        return cell
+    }
 }

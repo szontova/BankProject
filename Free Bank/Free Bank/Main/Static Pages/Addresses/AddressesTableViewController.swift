8d6f8ba61f2cc5437f731a76e28e7d9e379ed22b
Alexandra <shzontova@gmail.com>
2021-03-01 14:44:16 +0300

    add show function of ATMs and branches

--- Free Bank/Free Bank/Main/Static Pages/Addresses/AddressesTableViewController.swift
+++ Free Bank/Free Bank/Main/Static Pages/Addresses/AddressesTableViewController.swift
@@ -6,85 +6,61 @@
 //
 
 import UIKit
+import CoreData
+
+private let context: NSManagedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
 class AddressesTableViewController: UITableViewController {
-
+    
+    var branches = [Branch]()
+    var atms = [ATM]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.tableView.backgroundView = UIImageView(image: UIImage(named: "mainBackground"))
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        let branchesRequest = Branch.fetchRequest() as NSFetchRequest<Branch>
+        let atmsRequest = ATM.fetchRequest() as NSFetchRequest<ATM>
+        
+        do {
+            branches = try context.fetch(branchesRequest)
+            atms = try context.fetch(atmsRequest)
+            
+            print(branches.count)
+            print(atms.count)
+        } catch {
+            print("AddressesTableViewController: Error in get addresses.")
+        }
+    }
+   
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+        label.backgroundColor = UIColor.black
+        label.textColor = UIColor.white
+        label.text =  section == 0 ? "Отделы" : "Банкоматы"
+        return label
     }
 
-    // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        if section == 0 {
+            return branches.count
+        }
+        return atms.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "address", for: indexPath)
+        cell.textLabel?.text = indexPath.section == 0 ? branches[indexPath.row].address : atms[indexPath.row].address
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    
 
 }

1dd7e7504cb5167c6fe9c54ac0435b90d1d5adfa
Valeriya <valeri1605@mail.ru>
2021-03-02 15:48:13 +0300

    added message if account doesn't have cards

--- Free Bank/Free Bank/Home/Cards/AccountCardsViewController.swift
+++ Free Bank/Free Bank/Home/Cards/AccountCardsViewController.swift
@@ -12,9 +12,11 @@ class AccountCardsViewController: UIViewController {
     @IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet weak var accountNumberLabel: UILabel!
     @IBOutlet weak var accountBalanceLabel: UILabel!
+    @IBOutlet weak var missingCardsLabel: UILabel!
+    
     
     private var account: Account?
-
+    private var cards: [Card] = []
     
     func setAccount(_ acc: Account?) {
         self.account = acc
@@ -33,8 +35,17 @@ class AccountCardsViewController: UIViewController {
         
         accountNumberLabel.text = "Cчёт: \(account?.idNumber ?? "")"
         accountBalanceLabel.text = NSString(format: "Баланс: %.2f BYR", account?.balance ?? 0.0) as String
+        
+        updateCards()
+        
+        if cards.isEmpty { missingCardsLabel.isHidden = false }
     }
 
+    func updateCards() {
+        let cardsSet = account?.cards
+        cards = Array ( cardsSet as! Set<Card> )
+    }
+    
     @IBAction func addCardButton(_ sender: UIButton) {
     }
 }

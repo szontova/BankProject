b5b7b157a9286e1ea6060d9675e004f77cec92f8
Valeriya <valeri1605@mail.ru>
2021-03-02 17:22:33 +0300

    beauty cardsTableView

--- Free Bank/Free Bank/Home/Cards/AccountCardsViewController.swift
+++ Free Bank/Free Bank/Home/Cards/AccountCardsViewController.swift
@@ -13,6 +13,7 @@ class AccountCardsViewController: UIViewController {
     @IBOutlet weak var accountNumberLabel: UILabel!
     @IBOutlet weak var accountBalanceLabel: UILabel!
     @IBOutlet weak var missingCardsLabel: UILabel!
+    @IBOutlet weak var cardsTableView: UITableView!
     
     
     private var account: Account?
@@ -35,10 +36,13 @@ class AccountCardsViewController: UIViewController {
         accountNumberLabel.text = "Cчёт: \(account?.idNumber ?? "")"
         accountBalanceLabel.text = NSString(format: "Баланс: %.2f BYR", account?.balance ?? 0.0) as String
         missingCardsLabel.isHidden = true
+        cardsTableView.isHidden = true
         
         updateCards()
-        
+             
         if cards.isEmpty { missingCardsLabel.isHidden = false }
+        else { cardsTableView.isHidden = false }
+        cardsTableViewConfigurations()
     }
 
     func updateCards() {
@@ -46,11 +50,52 @@ class AccountCardsViewController: UIViewController {
         cards = Array ( cardsSet as! Set<Card> )
     }
     
+    
+    func cardsTableViewConfigurations(){
+        
+        cardsTableView.backgroundColor = .clear
+        
+        cardsTableView.register(CardTableViewCell.nib(), forCellReuseIdentifier: CardTableViewCell.identifier)
+        
+        cardsTableView.delegate = self
+        cardsTableView.dataSource = self
+        
+    }
+    
     @IBAction func addCardButton(_ sender: UIButton) {
         if let acc = account { addCard(acc) }
+        
         updateCards()
         
-        if cards.isEmpty { missingCardsLabel.isHidden = false }
-        else { missingCardsLabel.isHidden = true }
+        if cards.isEmpty {
+            missingCardsLabel.isHidden = false
+            cardsTableView.isHidden = true
+        }
+        else {
+            missingCardsLabel.isHidden = true
+            cardsTableView.isHidden = false
+        }
+        
+        DispatchQueue.main.async {
+            self.cardsTableView.reloadData()
+        }
     }
+}
+
+extension AccountCardsViewController: UITableViewDelegate {}
+
+extension AccountCardsViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cards.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = self.cardsTableView.dequeueReusableCell(withIdentifier: CardTableViewCell.identifier) as? CardTableViewCell {
+            cell.configure(with: cards[indexPath.row])
+            return cell
+        }
+        return UITableViewCell()
+    }
+
 }

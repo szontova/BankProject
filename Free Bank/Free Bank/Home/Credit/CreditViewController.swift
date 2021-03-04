76eb87f441c31acde8b1b9b679816c2be462bdd6
Valeriya <valeri1605@mail.ru>
2021-03-04 20:47:56 +0300

    print datas in credit page

--- Free Bank/Free Bank/Home/Credit/CreditViewController.swift
+++ Free Bank/Free Bank/Home/Credit/CreditViewController.swift
@@ -8,7 +8,19 @@
 import UIKit
 
 class CreditViewController: UIViewController {
+    
     @IBOutlet private weak var navigationBar: UINavigationBar!
+    @IBOutlet weak var creditNumberLabel: UILabel!
+    @IBOutlet weak var accountNumberLabel: UILabel!
+    @IBOutlet weak var balanceAccountLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var procentLabel: UILabel!
+    @IBOutlet weak var beginDateLabel: UILabel!
+    @IBOutlet weak var monthlyPayLabel: UILabel!
+    @IBOutlet weak var endDateLabel: UILabel!
+    @IBOutlet weak var countOfPaymentsLabel: UILabel!
+    @IBOutlet weak var nextPayDateLabel: UILabel!
+    
     private var credit: Credit?
     
     func setCredit(_ credit: Credit) {
@@ -18,7 +30,21 @@ class CreditViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         transparentNavBar(navigationBar)
-        print(credit)
+        if let cred = credit {
+            creditNumberLabel.text = String(cred.idNumber)
+            if let acc = cred.account{
+                accountNumberLabel.text = "Счёт: \(acc.idNumber!)"
+                balanceAccountLabel.text =  NSString(format: "%.2f BYR", Float(acc.balance)/100) as String
+            }
+            amountLabel.text = NSString(format: "%.2f BYR", Float(cred.amount)/100) as String
+            procentLabel.text = "\(cred.procent) %"
+         
+            beginDateLabel.text = Util.getddMMyyyyDateString(cred.date!)
+            let endDate = Util.getIntervalDate(cred.date!, Float(cred.term)/12)
+            endDateLabel.text = Util.getddMMyyyyDateString(endDate)
+            
+            monthlyPayLabel.text = NSString(format: "%.2f BYR", Float(Util.calculateMonthlyPay(amount: cred.amount, term: cred.term, procent: cred.procent))/100) as String
+        }
     }
     
 }
--- Free Bank/Free Bank/Utils/UtilityFunctions.swift
+++ Free Bank/Free Bank/Utils/UtilityFunctions.swift
@@ -32,6 +32,16 @@ public class Util {
         return dateFormatter.string(for: validDate) ?? "00/00"
     }
     
+    static func getddMMyyyyDateString(_ date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        return dateFormatter.string(for: date) ?? "00.00.0000"
+    }
+    
+    static func getIntervalDate(_ date: Date, _ years: Float) -> Date {
+       return NSDate(timeInterval: TimeInterval(years * 365 * 24 * 60 * 60) , since: date) as Date
+    }
+    
     static func callNumber(number: String) {
         if let url: NSURL = URL(string: "TEL://" + number) as NSURL?  { UIApplication.shared.open(url as URL, options: [:], completionHandler: nil)}
     }

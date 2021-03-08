736792dc0abd99bf89b49ef97596bd2a93c48cc5
Valeriya <valeri1605@mail.ru>
2021-03-08 17:38:35 +0300

    connected NewTransaction with ConfirmNewTransaction

--- Free Bank/Free Bank/Home/NewTransaction/ConfirmationTransactionViewController.swift
+++ Free Bank/Free Bank/Home/NewTransaction/ConfirmationTransactionViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class ConfirmationTransactionViewController: UIViewController {
 
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        transparentNavBar(navigationBar)
     }
     
 }

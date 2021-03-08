3a20d0b43dd9120f195c5a0f3a07820574fe95cc
Valeriya <valeri1605@mail.ru>
2021-03-08 17:29:35 +0300

    added ConfirmationTransactionVC

---
+++ Free Bank/Free Bank/Home/NewTransaction/ConfirmationTransactionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ConfirmationTransactionViewController.swift
+//  Free Bank
+//
+//  Created by Пользователь on 8.03.21.
+//
+
+import UIKit
+
+class ConfirmationTransactionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}

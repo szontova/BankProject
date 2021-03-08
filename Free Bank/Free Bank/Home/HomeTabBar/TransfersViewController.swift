874550faf8ffd8463a340e258d28bd24ec35fc86
Valeriya <valeri1605@mail.ru>
2021-03-08 13:25:01 +0300

    added pickerView in Transfers page

--- Free Bank/Free Bank/Home/HomeTabBar/TransfersViewController.swift
+++ Free Bank/Free Bank/Home/HomeTabBar/TransfersViewController.swift
@@ -13,14 +13,56 @@ class TransfersViewController: UIViewController {
     @IBOutlet weak var navigationBar: UINavigationBar!
     @IBOutlet weak var senderSegmentedControl: UISegmentedControl!
     @IBOutlet weak var receiverSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var accountsPickerView: UIPickerView!
+    @IBOutlet weak var missingTransfersLabel: UILabel!
     
     private var individual: Individual?
     private var organization: Organization?
 
+    private var accounts: [Account] = []
+    private var transfers: [Transaction] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+    
         transparentNavBar(navigationBar)
+        accountsPickerViewConfigurations()
+    }
+    
+    func accountsPickerViewConfigurations(){
+        
+        accountsPickerView.delegate = self
+        accountsPickerView.dataSource = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        updateAccounts()
+    }
+    
+    func updateAccounts(){
+        let accs = individual?.accounts ?? organization?.accounts
+        accounts = Array ( accs as! Set<Account> )
+       
+        accounts.sort(){
+            return $0.idNumber! < $1.idNumber!
+        }
+    }
+    
+    func updateTransfers(_ account: Account) {
+        transfers = Array ( account.transactions as! Set<Transaction> )
+       
+        transfers.sort(){
+            return $0.date! < $1.date!
+        }
+    }
+    
+    func  updateTransfersTableView(){
+        if transfers.isEmpty {
+            missingTransfersLabel.isHidden = false
+        }
+        else {
+            missingTransfersLabel.isHidden = true
+        }
     }
     
     @IBAction func addTransaction(_ sender: UIButton) {
@@ -52,4 +94,39 @@ extension TransfersViewController: OrgIndivid {
         self.organization = org
     }
     
+}
+
+extension TransfersViewController: UIPickerViewDelegate{}
+extension TransfersViewController: UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+       return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return accounts.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let label = UILabel()
+        label.backgroundColor = UIColor.init(red: 181/255, green: 150/255, blue: 142/255, alpha: 1.0)
+        label.textColor = UIColor.white
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: label.font.pointSize)
+        
+        label.layer.frame = CGRect(x: 0,y: 0, width: pickerView.frame.width - 20, height: 24 )
+        label.layer.masksToBounds = true
+        label.layer.cornerRadius = 5
+        
+        label.text = accounts[row].idNumber
+        return label
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 27.5
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        updateTransfers(accounts[row])
+        updateTransfersTableView()
+    }
 }

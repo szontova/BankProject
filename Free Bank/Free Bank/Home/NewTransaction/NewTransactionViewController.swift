6f2564564f5e81f53406412cb1e977e791b53c77
Valeriya <valeri1605@mail.ru>
2021-03-09 10:28:45 +0300

    added accountsPickerView

--- Free Bank/Free Bank/Home/NewTransaction/NewTransactionViewController.swift
+++ Free Bank/Free Bank/Home/NewTransaction/NewTransactionViewController.swift
@@ -17,7 +17,7 @@ class NewTransactionViewController: UIViewController {
     @IBOutlet weak var senderCardValidity: UITextField!
     @IBOutlet weak var senderCardCVV: UITextField!
     
-    @IBOutlet weak var senderAccountTextField: UITextField!
+    @IBOutlet weak var senderAccountPickerView: UIPickerView!
     
     @IBOutlet weak var receiverTextField: UITextField!
     
@@ -25,6 +25,8 @@ class NewTransactionViewController: UIViewController {
     private var organization: Organization?
     private var senderType: (Bool, Bool) = (false,false)
     private var receiverType: (Bool, Bool) = (false,false)
+    
+    private var accounts: [Account] = []
     
     func setSender( card: Bool,  acc: Bool){
         self.senderType = (card, acc)
@@ -38,10 +40,12 @@ class NewTransactionViewController: UIViewController {
         super.viewDidLoad()
         
         transparentNavBar(navigationBar)
+        senderAccountPickerViewConfigurations()
     }
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard segue.identifier == "toConfirmationTransactionSegue" else { return }
-        guard let destinationVC = segue.destination as? ConfirmationTransactionViewController else { return }
+        guard let _ = segue.destination as? ConfirmationTransactionViewController else { return }
 //        destinationVC.setSender(card: <#T##Card?#>, acc: <#T##Account?#>)
 //        destinationVC.setReceiver(card: <#T##Card?#>, acc: <#T##Account?#>)
     }
@@ -51,6 +55,7 @@ class NewTransactionViewController: UIViewController {
         if senderType.1 {
             cardView.isHidden = true
             accountView.isHidden = false
+            updateAccounts()
         } else {
             cardView.isHidden = false
             accountView.isHidden = true
@@ -67,6 +72,30 @@ class NewTransactionViewController: UIViewController {
         self.view.endEditing(true)
     }
     
+    func senderAccountPickerViewConfigurations(){
+        
+        senderAccountPickerView.delegate = self
+        senderAccountPickerView.dataSource = self
+    }
+    
+    
+    func updateAccounts(){
+        let accs = individual?.accounts ?? organization?.accounts
+        accounts = Array ( accs as! Set<Account> )
+       
+        accounts.sort(){
+            return $0.idNumber! < $1.idNumber!
+        }
+        
+       accounts = accounts.filter { (acc) -> Bool in
+            if let rev = acc.deposit?.revocable {
+                return rev
+            }
+            return true
+        }
+        print(accounts)
+    }
+    
     @IBAction func unwindToNewTransactionFromConfirmationTransaction(segue: UIStoryboardSegue){
         guard segue.identifier == "unwindToNewTransactionFromConfirmSegue" else {return}
         guard let _ = segue.destination as? ConfirmationTransactionViewController else {return}
@@ -78,10 +107,10 @@ class NewTransactionViewController: UIViewController {
             result = findCard(by: senderCardNumberTextField.text!) != nil ? true : false
             print(result)
         }
-        if senderType.1 {
-            result = findAccount(by: senderAccountTextField.text!) != nil ? true : false
-            print(result)
-        }
+//        if senderType.1 {
+//           // result = findAccount(by: senderAccountTextField.text!) != nil ? true : false
+//            print(result)
+//        }
         if result {
             performSegue(withIdentifier: "toConfirmationTransactionSegue", sender: nil)
         } else {showAlertError(message: "Неверно введены данные отправителя.")}
@@ -104,4 +133,35 @@ extension NewTransactionViewController: UITextFieldDelegate{
         return true
     }
     
+}
+
+extension NewTransactionViewController: UIPickerViewDelegate {}
+extension NewTransactionViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+       return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return accounts.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+        let label = UILabel()
+        //label.backgroundColor = UIColor.init(red: 181/255, green: 150/255, blue: 142/255, alpha: 1.0)
+        label.backgroundColor = UIColor.init(red: 96/255, green: 3/255, blue: 0/255, alpha: 1.0)
+        label.textColor = UIColor.white
+        label.textAlignment = .center
+        //label.font = UIFont.boldSystemFont(ofSize: label.font.pointSize)
+        
+        label.layer.frame = CGRect(x: 0,y: 0, width: pickerView.frame.width - 20, height: 26 )
+        label.layer.masksToBounds = true
+        label.layer.cornerRadius = 5
+        
+        label.text = accounts[row].idNumber
+        return label
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 27.5
+    }
 }
